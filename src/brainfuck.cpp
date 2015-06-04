97696728417f32973e76a23c30084cbca8b2d75d
Chereigna Allen <allenc3@wit.edu>
2015-06-04 14:39:25 -0400

    Lab 3 done

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -182,27 +182,39 @@ class Interpreter : public Visitor {
         void visit(const CommandNode * leaf) {
             switch (leaf->command) {
                 case INCREMENT:
+                    memory[pointer]++;
                     break;
                 case DECREMENT:
+                    memory[pointer]--;
                     break;
                 case SHIFT_LEFT:
+                    pointer --;
                     break;
                 case SHIFT_RIGHT:
+                    pointer ++;
                     break;
                 case INPUT:
+                    cin.get(memory[pointer]);
                     break;
                 case OUTPUT:
+                    cout << memory[pointer];
                     break;
             }
         }
         void visit(const Loop * loop) {
-            for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
+            while(memory[pointer] != 0){
+                for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
                 (*it)->accept(this);
+                }
             }
         }
         void visit(const Program * program) {
             // zero init the memory array
+            for (int i = 0; i < 30000;i++){
+                memory[i] = 0;
+            }
             // set pointer to zero
+            pointer = 0;
             for (vector<Node*>::const_iterator it = program->children.begin(); it != program->children.end(); ++it) {
                 (*it)->accept(this);
             }
